30f8196bd1f6f0a404b114da6ab93cf8c8a6b7bd
4015b12 <4015b12@guayaba.ece.uprm.edu>
2017-02-08 10:22:21 -0400

    Test

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -3,7 +3,7 @@ import javax.swing.JFrame;
 public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Christian");
-		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
 		myFrame.setLocationRelativeTo(null);
